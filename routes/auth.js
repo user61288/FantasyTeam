8f41e9703be26cccbb9c3ed3cbf2868a70ab11cd
Aruna Vallur <vallur.aruna@gmail.com>
2017-11-27 19:11:43 -0800

    update logout

--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,21 @@ module.exports = function(app, passport) {
         successRedirect: '/dashboard',
  
         failureRedirect: '/signup'
-    }
+    }));
 
-    // app.post('/signin')
- 
-));
+	app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/signin'
+    }));
+    
+    app.get('/logout',authController.logout);
 
+    function isLoggedIn(req, res, next) {
+ 		if (req.isAuthenticated())
+     		return next();
+        res.redirect('/signin');
+	}
+
+	app.get('/dashboard',isLoggedIn, authController.dashboard);
 }
 

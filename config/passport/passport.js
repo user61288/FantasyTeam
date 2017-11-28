d61394f67134fd75bb7c9d5a5190a879cb722698
Aruna Vallur <vallur.aruna@gmail.com>
2017-11-27 18:59:03 -0800

    force add passport.js even if ignored in gitignore

---
+++ config/passport/passport.js
@@ -0,0 +1,68 @@
+var bCrypt = require('bcrypt-nodejs');
+
+module.exports = function(passport, user) {
+
+  var User = user;
+  var LocalStrategy = require('passport-local').Strategy;
+  
+  console.log("User", user);
+  console.log("LocalStrategy", LocalStrategy);
+  
+  passport.use('local-signup', new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true 
+    },
+    function(req, email, password, done) {
+      var generateHash = function(password) {
+        return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+      };
+      User.findOne({
+        where: {
+	         email: email
+	      }
+      }).then(function(user) {
+      
+      if (user)
+      {
+	      return done(null, false, { message: 'That email is already taken' }); 
+	    } else 
+      { 
+	      var userPassword = generateHash(password); 
+	      var data = { 
+	                 email: email, 
+	                 password: userPassword,
+	                 name: req.body.name,
+                   teamName: "team1"
+                  };
+
+        User.create(data).then(function(newUser, created) { 
+	        if (!newUser) { 
+	           return done(null, false); 
+	        }  
+          else if (newUser) { 
+	           return done(null, newUser); 
+          } 
+	      });
+      }
+    });
+  }));
+
+  //serialize
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  // deserialize user 
+  passport.deserializeUser(function(id, done) {
+    User.findById(id).then(function(user) {
+      if (user) {
+        done(null, user.get());
+      } else {
+        done(user.errors, null);
+      }
+    });
+  });
+}
+
